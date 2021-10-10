6d79b08b3eea000a25552ed24a0babbd931c553a
Gunnar Voet <gvoet@ucsd.edu>
2021-10-10 13:08:02 +0000

    Bump version: 0.1.4 → 0.1.5

--- ctdproc/__init__.py
+++ ctdproc/__init__.py
@@ -3,6 +3,6 @@ Library for CTD data processing.
 """
 
 __all__ = ["io", "proc", "calcs", "helpers"]
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from . import calcs, helpers, io, proc
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("HISTORY.rst") as history_file:
 setup(
     # Project information
     name="ctdproc",
-    version="0.1.4",
+    version="0.1.5",
     author="Gunnar Voet",
     author_email="gvoet@ucsd.edu",
     url="https://github.com/gunnarvoet/ctdproc",
