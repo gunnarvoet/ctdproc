02a93355ae9f3ccd8e4bf57180c279a98adf434b
Gunnar Voet <gvoet@ucsd.edu>
2023-10-13 21:57:47 -0700

    clean up testing

--- ctdproc/tests/test_basic.py
+++ ctdproc/tests/test_basic.py
@@ -5,33 +5,28 @@ import numpy as np
 import ctdproc as ctd
 
 
-def test_success():
-    assert True
-
-
 # We defined rootdir as a fixture in conftest.py
 # and can use it here as input now
 def test_read_hex(rootdir, tmpdir):
     hexfile = rootdir / "data/BLT_Test_001.hex"
     assert type(hexfile) == pathlib.PosixPath
-    print(hexfile)
     assert hexfile.exists()
     c = ctd.io.CTDHex(hexfile)
-
     cx = c.to_xarray()
 
-    # make sure we can write and read the data as netcdf
-    p = pathlib.Path(tmpdir) / "testfile.nc"
-    cx.to_netcdf(p)
-    cx2 = xr.open_dataset(p)
-    assert type(cx2) == xr.core.dataset.Dataset
+    # # make sure we can write and read the data as netcdf
+    # # for some reason this results in weird warnings in pytest
+    # # not doing this for now
+    # p = pathlib.Path(tmpdir) / "testfile.nc"
+    # cx.to_netcdf(p)
+    # cx2 = xr.open_dataset(p)
+    # assert type(cx2) == xr.core.dataset.Dataset
 
 
-def test_read_lajit(rootdir, tmpdir):
+def test_read_lajit(rootdir):
     """Read test data from La Jolla Canyon."""
     hexfile = rootdir / "data/lajit2-sr1614-001.hex"
     assert type(hexfile) == pathlib.PosixPath
-    print(hexfile)
     assert hexfile.exists()
     c = ctd.io.CTDHex(hexfile)
 
